e04f864b9c5d8939123036ea218dbb801ba11219
PatrissolJuns <patrissolkenfack@gmail.com>
2020-11-22 23:21:52 +0100

    Insert Programming language

---
+++ src/components/programming-icons/data.js
@@ -0,0 +1,39 @@
+import Php from './Php';
+import React from 'react';
+import Java from './Java';
+import Python from './Python';
+import CPlusPlus from './CPlusPlus';
+import JavaScript from './JavaScript';
+
+module.exports = [
+    {
+        id: 0,
+        name: 'JavaScript',
+        component: <JavaScript />,
+        color: '',
+    },
+    {
+        id: 1,
+        name: 'Python',
+        component: <Python />,
+        color: '',
+    },
+    {
+        id: 2,
+        name: 'Php',
+        component: <Php />,
+        color: '',
+    },
+    {
+        id: 3,
+        name: 'Java',
+        component: <Java />,
+        color: '',
+    },
+    {
+        id: 4,
+        name: 'CPlusPlus',
+        component: <CPlusPlus />,
+        color: '',
+    },
+];
