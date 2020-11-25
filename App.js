ad4202587cf9bec64ab085160d464eec232e3242
PatrissolJuns <patrissolkenfack@gmail.com>
2020-11-25 05:39:15 +0100

    Added some packages and init moment

--- App.js
+++ App.js
@@ -1,6 +1,9 @@
 import 'react-native-gesture-handler';
 import React, {Component} from 'react';
 import MainNavigator from "./src/navigations";
+import initMoment from './src/service/momentService';
+
+initMoment();
 
 class App extends Component {
   render() {
