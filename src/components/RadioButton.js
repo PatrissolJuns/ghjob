cfeb22d128d217fed8e740454b79bfb7f848f988
PatrissolJuns <patrissolkenfack@gmail.com>
2021-01-31 20:48:55 +0100

    Created RadioButton Component

---
+++ src/components/RadioButton.js
@@ -0,0 +1,100 @@
+import PropTypes from 'prop-types';
+import React, {useState} from 'react';
+import {Text, TouchableOpacity, View} from 'react-native';
+
+/**
+ * Custom RadioButton Component
+ * @param props
+ * @returns {*}
+ * @constructor
+ */
+const RadioButton = props => {
+    const {
+        options,
+        onChange,
+        checkedColor,
+        containerStyle,
+        defaultSelected,
+    } = props;
+
+    const [active, setActive] = useState(defaultSelected ? defaultSelected : options[0]);
+
+    const onOptionPress = (currentOption) => {
+        if (currentOption.value !== active.value) {
+            setActive(currentOption);
+            onChange(currentOption);
+        }
+    };
+
+    return (
+        <View>
+            {options.map((option, key) => {
+                const isActive = active.value === option.value;
+
+                return (
+                    <TouchableOpacity
+                        key={key}
+                        activeOpacity={0.7}
+                        onPress={() => onOptionPress(option)}
+                        style={{
+                            flexDirection: "row",
+                            alignItems: "center",
+                            marginBottom: 5
+                        }}
+                    >
+                        <View
+                            style={[
+                                {
+                                    height: 24,
+                                    width: 24,
+                                    borderRadius: 12,
+                                    borderWidth: 2,
+                                    borderColor: checkedColor,
+                                    alignItems: 'center',
+                                    justifyContent: 'center',
+                                    marginRight: 10
+                                },
+                                containerStyle
+                            ]}
+                        >
+                            {
+                                isActive ? (
+                                    <View style={{
+                                        height: 12,
+                                        width: 12,
+                                        borderRadius: 6,
+                                        backgroundColor: checkedColor,
+                                    }}/>
+                                ) : null
+                            }
+                        </View>
+                        <Text>{option.name}</Text>
+                    </TouchableOpacity>
+                )
+            })}
+        </View>
+    );
+};
+
+RadioButton.propTypes = {
+    onChange: PropTypes.func.isRequired,
+    checkedColor: PropTypes.string,
+    containerStyle: PropTypes.object,
+    options: PropTypes.arrayOf(
+        PropTypes.shape({
+            name: PropTypes.string.isRequired,
+            value: PropTypes.string.isRequired,
+        })
+    ).isRequired,
+    defaultSelected: PropTypes.shape({
+        name: PropTypes.string.isRequired,
+        value: PropTypes.string.isRequired,
+    })
+};
+
+RadioButton.defaultProps = {
+    containerStyle: {},
+    checkedColor: '#FF7764',
+};
+
+export default RadioButton;
