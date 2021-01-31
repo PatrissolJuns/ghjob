71a0f4ce9c560ff3c93d1f6e4845f5442d16ce5c
PatrissolJuns <patrissolkenfack@gmail.com>
2021-01-31 20:54:50 +0100

    Created Job Class

---
+++ src/models/Job.js
@@ -0,0 +1,21 @@
+import moment from 'moment';
+import {removeTags} from '../service/helper';
+
+export class Job {
+    constructor(job) {
+        this.id = job.id;
+        this.type = job.type;
+        this.url = job.url;
+        this.company = {
+            name: job.company,
+            url: job.company_url,
+            logo: job.company_logo,
+        };
+        this.title = job.title;
+        this.location = job.location;
+        this.description = job.description;
+        this.descNoTags = removeTags(job.description);
+        this.howToApply = job.how_to_apply;
+        this.createdAt = moment(new Date(job.created_at));
+    }
+}
