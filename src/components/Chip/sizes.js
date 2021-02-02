5fcdff53db88c4cb18929c8d544ca1b65315ddc2
PatrissolJuns <patrissolkenfack@gmail.com>
2021-02-02 11:15:40 +0100

    Created Material Chip Component

---
+++ src/components/Chip/sizes.js
@@ -0,0 +1,11 @@
+import { PixelRatio } from 'react-native'
+
+export const CHIP_HEIGHT = PixelRatio.roundToNearestPixel(32)
+export const CHIP_RADIUS = PixelRatio.roundToNearestPixel(16)
+export const CHIP_MARGIN = PixelRatio.roundToNearestPixel(4)
+export const CHIP_TEXT_SIZE = PixelRatio.roundToNearestPixel(14)
+export const CHIP_TEXT_MARGIN = PixelRatio.roundToNearestPixel(16)
+export const CHIP_LEFT_ICON_SIZE = PixelRatio.roundToNearestPixel(24)
+export const CHIP_LEFT_ICON_RADIUS = PixelRatio.roundToNearestPixel(12)
+export const CHIP_RIGHT_ICON_SIZE = PixelRatio.roundToNearestPixel(18)
+export const CHIP_RIGHT_ICON_RADIUS = PixelRatio.roundToNearestPixel(9)
