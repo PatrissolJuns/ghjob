89e7357aa00ecc30c1f234333a9cada82f3e681a
PatrissolJuns <patrissolkenfack@gmail.com>
2021-02-02 13:07:43 +0100

    Styled Drawer Navigator

--- src/navigations/HomeNavigator.js
+++ src/navigations/HomeNavigator.js
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import {Colors} from '../styles';
 import Home from '../screens/Home';
 import {ABOUT, HOME} from '../urls/routes';
 import AboutScreen from '../screens/AboutScreen';
@@ -8,7 +9,13 @@ const Drawer = createDrawerNavigator();
 
 const HomeNavigator = () => {
     return (
-        <Drawer.Navigator initialRouteName={HOME}>
+        <Drawer.Navigator
+            initialRouteName={HOME}
+            drawerContentOptions={{
+                activeTintColor: Colors.PRIMARY,
+                activeBackgroundColor: 'rgba(255, 119, 100, 0.1)',
+            }}
+        >
             <Drawer.Screen name={HOME} component={Home} />
             <Drawer.Screen name={ABOUT} component={AboutScreen} />
         </Drawer.Navigator>
