f95ea8b5d86c98f6ef9d5990fd22a087669e2c15
PatrissolJuns <patrissolkenfack@gmail.com>
2021-02-16 08:25:23 +0100

    Improved FetchFailedComponent

--- src/components/FetchFailedComponent.js
+++ src/components/FetchFailedComponent.js
@@ -1,32 +1,86 @@
 import React from 'react';
+import FontText from './FontText';
 import PropTypes from 'prop-types';
-import {Typography} from '../styles';
-import {Text, View} from 'react-native';
+import {TEXT_PRIMARY} from '../styles/colors';
+import CustomHeaderPage from './CustomHeaderPage';
+import NotFoundSvg from '../assets/images/not_found.svg';
+import {GENERAL_STYLE_SETTING, Typography} from '../styles';
+import {ScrollView, View, StyleSheet, TouchableOpacity} from 'react-native';
 
 /**
  * Display generic error and retry button when a request failed
  * @param onRetryClick
  * @param errorMessageComponent
+ * @param backOnClick
+ * @param backText
  * @returns {*}
  * @constructor
  */
-const FetchFailedComponent = ({onRetryClick, errorMessageComponent}) => {
+const FetchFailedComponent = ({onRetryClick, errorMessageComponent, backOnClick, backText}) => {
     return (
-        <View>
-            {errorMessageComponent ? (
-                <>
-                    {errorMessageComponent(onRetryClick)}
-                </>
-            ) : (
-                <Text>
-                    An error occur. <Text style={{...Typography.FONT_BOLD}} onPress={onRetryClick}>Please try again</Text>
-                </Text>
+        <ScrollView contentContainerStyle={style.container}>
+            {backOnClick && backText && (
+                <CustomHeaderPage
+                    title={backText}
+                    onBackPressed={backOnClick}
+                />
             )}
-        </View>
+            <View style={style.wrapper}>
+                <NotFoundSvg
+                    width={'100%'}
+                    height={200}
+                />
+                {errorMessageComponent ? (
+                    <>
+                        {errorMessageComponent(onRetryClick)}
+                    </>
+                ) : (
+                    <View style={{
+                        flexDirection: 'row',
+                        marginTop: 10,
+                    }}>
+                        <FontText style={style.textWrapper}>An error occur.</FontText>
+                        <TouchableOpacity
+                            onPress={onRetryClick}
+                            style={{paddingLeft: 3}}
+                            hitSlop={GENERAL_STYLE_SETTING.HIT_SLOP}
+                        >
+                            <FontText style={[style.textWrapper, style.click]}>Please try again</FontText>
+                        </TouchableOpacity>
+                    </View>
+                )}
+            </View>
+        </ScrollView>
     );
 };
 
+const style = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+    },
+    wrapper: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    textWrapper: {
+        fontSize: 20,
+        // marginTop: 10,
+        textAlign: 'center',
+        color: TEXT_PRIMARY,
+    },
+    click: {
+        ...Typography.FONT_BOLD,
+        // backgroundColor: 'red',
+        paddingLeft: 3,
+        textDecorationLine: 'underline'
+    }
+});
+
 FetchFailedComponent.propTypes = {
+    backText: PropTypes.string,
+    backOnClick: PropTypes.func,
     errorMessageComponent: PropTypes.any,
     onRetryClick: PropTypes.func.isRequired,
 };
